7a8c5664cd34b31b3e5197c2306fdb2d2cd64a55
Wu Xueyao <xueyaowu@aliyun.com>
2024-04-28 14:21:13 +0800

    feat: add generator&iterator

---
+++ demos/generator-iterator.js
@@ -0,0 +1,41 @@
+/**
+ * 生成器
+ */
+
+// 简单生成器
+function* numberGenerator(n) {
+    let i = 1;
+    while (i <= n) {
+        yield i;
+        i++;
+    }
+}
+// 使用生成器
+const generator = numberGenerator(5);
+for (const num of generator) {
+    console.log(num);
+}
+
+
+/**
+ * 迭代器
+ */
+
+// 简单迭代器
+const simpleIterator = {
+    data: [1, 2, 3, 4, 5],
+    index: 0,
+    [Symbol.iterator]() {
+        return this;
+    },
+    next() {
+        if (this.index >= this.data.length) {
+            return { done: true };
+        }
+        return { value: this.data[this.index++], done: false };
+    }
+};
+// 使用迭代器
+for (const item of simpleIterator) {
+    console.log(item);
+}
